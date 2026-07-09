196f0f5a3064bd2c8df11daeb231c1860f0cd6ae
taaaruunnn <taaaruunnn@gmail.com>
2026-07-10 00:28:12 +0530

    Setup project structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,73 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AuthLayout } from './layout/auth-layout/auth-layout';
+import { MainLayout } from './layout/main-layout/main-layout';
+
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Profile } from './pages/profile/profile';
+import { Search } from './pages/search/search';
+import { Upload } from './pages/upload/upload';
+import { Admin } from './pages/admin/admin';
+import { Settings } from './pages/settings/settings';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        component: Login,
+      },
+      {
+        path: 'register',
+        component: Register,
+      },
+    ],
+  },
+
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard,
+      },
+      {
+        path: 'profile',
+        component: Profile,
+      },
+      {
+        path: 'search',
+        component: Search,
+      },
+      {
+        path: 'upload',
+        component: Upload,
+      },
+      {
+        path: 'admin',
+        component: Admin,
+      },
+      {
+        path: 'settings',
+        component: Settings,
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
