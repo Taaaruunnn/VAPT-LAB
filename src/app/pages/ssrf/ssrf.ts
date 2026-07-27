e94ba43179255bba6358579827dcc73b9cfadfd4
taaaruunnn <taaaruunnn@gmail.com>
2026-07-28 01:44:40 +0530

    polished ssrf html

--- src/app/pages/ssrf/ssrf.ts
+++ src/app/pages/ssrf/ssrf.ts
@@ -2,14 +2,36 @@ import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SsrfService } from '../../core/services/ssrf.service';
-
+import {
+  LucideAngularModule,
+  ShieldAlert,
+  Globe,
+  Server,
+  Link,
+  ArrowRight,
+  BadgeCheck,
+  Clock3,
+  FileJson,
+  CircleAlert,
+  Lightbulb
+} from 'lucide-angular';
 @Component({
   selector: 'app-ssrf',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, LucideAngularModule],
   templateUrl: './ssrf.html'
 })
 export class Ssrf {
+readonly ShieldAlert = ShieldAlert;
+readonly FileJson = FileJson;
+readonly Globe = Globe;
+readonly Server = Server;
+readonly Link = Link;
+readonly ArrowRight = ArrowRight;
+readonly BadgeCheck = BadgeCheck;
+readonly Clock3 = Clock3;
+readonly CircleAlert = CircleAlert;
+readonly Lightbulb = Lightbulb;
 
   private ssrfService = inject(SsrfService);
 
